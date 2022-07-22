705a8599c752abb639a20820b9a2ff0fb73480fe
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-07-22 12:05:00 +0200

    make asserting on floating point values

---
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
+        }
+
+    }
+}
